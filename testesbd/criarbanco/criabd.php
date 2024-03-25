d131a622466f449b252a54cc549419a4cdf93e86
Eduardo Miguel dos Reis Teixeira <133522018+EduardoMRT@users.noreply.github.com>
2024-03-25 09:32:07 -0300

    Update criabd.php

--- testesbd/criarbanco/criabd.php
+++ testesbd/criarbanco/criabd.php
@@ -1,14 +1,14 @@
 <?php
  //CRIA UM BANCO DE DADOS + TABELA
 $conn = mysqli_query("localhost", "admin", "");
-
  $sql1 = "CREATE DATABASE testebd";
+mysqli_query($conn, $sql1);
+
+$conn = mysqli_query("localhost", "admin", "", "testebd");
  $sql2 = "CREATE TABLE usuarios(
     id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     nome TEXT NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     senha VARCHAR(216) NOT NULL);"
-
-mysqli_query($conn, $sql1);
 mysqli_query($conn, $sql2);
  ?>
